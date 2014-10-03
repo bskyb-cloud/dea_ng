61596e2e05387fce6dfbef5cabcffcd3e06a872c
Ben Corlett <ben.corlett@gmail.com>
2014-10-03 17:34:08 +0100

    More mistakes

--- lib/dea/starting/database_uri_generator.rb
+++ lib/dea/starting/database_uri_generator.rb
@@ -8,8 +8,9 @@ module Dea
       'informix' => 'ibmdb'
     }.freeze
 
-    def initialize(services)
+    def initialize(services, zone)
       @services = Array(services).compact || []
+      @zone = zone
     end
 
     def database_uri
@@ -25,22 +26,14 @@ module Dea
         nil
       end
     end
-
-    def instance_zone
-      if (bootstrap.config['placement_properties'])
-        bootstrap.config['placement_properties']['zone']
-      else
-        ""
-      end
-    end
     
     def bound_relational_valid_databases
       @bound_relational_valid_databases ||= @services.inject([]) do |collection, binding|
         credentials = nil
         if binding["credentials"].has_key?('uri')
           credentials = binding["credentials"]
-        elsif binding['credentials'].has_key?(instance_zone) && binding['credentials'][instance_zone].has_key?('uri')
-          credentials = binding["credentials"][instance_zone]
+        elsif binding['credentials'].has_key?(@zone) && binding['credentials'][@zone].has_key?('uri')
+          credentials = binding["credentials"][@zone]
         end
           
         if credentials && credentials["uri"]
